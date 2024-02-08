90da2e8e4fa0b7340044b2c177efd5ba4264a786
DEARn6sx <nantawats.study@gmail.com>
2024-02-08 12:01:29 +0700

    improt image

---
+++ src/components/imgset.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-card  width="300" >
+        <v-img width="300" height="250" src="https://cdn.pixabay.com/photo/2024/01/08/15/54/defile-8495836_1280.jpg" />
+          <v-img width="300" height="250" src="https://cdn.pixabay.com/photo/2024/01/08/15/54/defile-8495836_1280.jpg" />
+    
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/me.vue
+++ src/views/me.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <h1>Yooo!! และนี่คือเสียงของเด็กวัด</h1>
+    <imgset />
   </div>
   
 </template>
 
 <script>
+import   imgset  from '../components/imgset.vue';
 export default {
-
+    components:{
+        imgset
+    }
 }
 </script>
 
