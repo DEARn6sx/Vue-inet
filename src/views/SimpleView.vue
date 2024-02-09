51d6b3d932ddee7ee3c5742ebbabf9176da6b601
DEARn6sx <nantawats.study@gmail.com>
2024-02-09 14:30:46 +0700

    Loop and v-card-title

--- src/views/SimpleView.vue
+++ src/views/SimpleView.vue
@@ -14,6 +14,22 @@
       :src="imgset"
     />
     <v-btn color="success" @click="show = !show">switch</v-btn>
+    <h1>Loop zone</h1>
+    <v-row>
+        <v-col cols="3" v-for="(item,index) in items" :key="index">
+            <v-card>
+                <v-img    
+                    width="300" 
+                    height="250"
+                    :src="item.img"
+                />
+                <v-card-title primary-title>
+                    {{item.meassage}}
+                </v-card-title>
+            </v-card>
+        </v-col>
+    </v-row>
+    
 
   </div>
 </template>
@@ -25,7 +41,21 @@ export default {
             name: "DEAR",
             imgUrl: 'https://cdn.pixabay.com/photo/2024/01/27/18/24/squirrel-8536537_1280.jpg',
             imgset: require('../assets/profile.png'),
-            show: false
+            show: false,
+            items: [
+                {
+                    meassage: 'Dooo',
+                    img: 'https://cdn.pixabay.com/photo/2024/01/27/18/24/squirrel-8536537_1280.jpg'
+                },
+                {
+                    meassage: 'Cooo',
+                    img: 'https://cdn.pixabay.com/photo/2024/01/27/18/24/squirrel-8536537_1280.jpg'
+                },
+                {
+                    meassage: 'Zooo',
+                    img: 'https://cdn.pixabay.com/photo/2024/01/27/18/24/squirrel-8536537_1280.jpg'
+                },
+            ]
         }
     }
 }
